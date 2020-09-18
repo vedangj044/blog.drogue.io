2136dad74eaa443d575a3abc898a079c3853fd3b
Jens Reimann <jreimann@redhat.com>
2020-09-18 11:49:56 +0200

    Drop unnecessary JS loading

--- themes/feather/static/js/images.js
+++ themes/feather/static/js/images.js
@@ -52,3 +52,21 @@ var images = (function() {
 
 	return images;
 }());
+
+var app = (function(){
+	app = {
+		init: function() {
+			// attach onload resize to images
+			var im = images.findAllImages();
+			var w = document.querySelector('.container').clientWidth;
+			images.attachLoadResizeHandler(im, w - 100);
+			images.stripParagraphFromImage(im);
+		}
+	}
+
+	window.onload = function(){
+		app.init();
+	};
+
+	return app;
+}());
